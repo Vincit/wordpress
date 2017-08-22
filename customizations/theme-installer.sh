f4210c4c25d359857ac0fe0ae584e8f5a3f52649
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-08-22 16:29:48 +0300

    Offer to activate theme

--- customizations/theme-installer.sh
+++ customizations/theme-installer.sh
@@ -16,6 +16,7 @@ theme_installer() {
   rootwd=$(pwd)
   read -r -p "==> Great! What name do you want to give to it? [A-Za-z0-9_] " themename
   read -r -p "==> And what is the repository URL?  (git@bitbucket...) " repo
+  read -r -p "==> Last thing! Would you like to activate the theme? (Y/n) " activate
 
   composer require vincit/wordpress-theme-base dev-master --prefer-source
   cd htdocs/wp-content/themes || exit 1
@@ -24,6 +25,7 @@ theme_installer() {
 
   recursive_replace "." "*" "wordpress-theme-base" "$themename"
 
+  # Keeping the history might prove useful sometime, don't delete .git folder!
   # git remote remove composer
   git remote remove origin
   git remote add origin "$repo"
@@ -34,7 +36,18 @@ theme_installer() {
   cd "$rootwd" || exit 1
   recursive_replace "$rootwd" "composer.json" "wordpress-theme-base" "$themename"
 
+  case "$activate" in
+    [nN][oO]|[nN])
+      echo "Skipping theme activation..."
+    ;;
+  *)
+    echo "Activating theme $themename..."
+    run "wp theme activate $themename"
+  esac
+
+
   echo "Theme generated. Assuming you provided a valid repository URL, the initial commit has been pushed."
   echo "Remember to add it to Private Packagist or Composer will refuse to work: https://packagist.com/orgs/vincit/packages/add"
+
 }
 
