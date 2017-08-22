6d74f71feb7dc9f5aabbd7704a6f5fd868ee0c42
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-08-22 13:41:48 +0300

    Prompt for repo url

--- customizations/theme-installer.sh
+++ customizations/theme-installer.sh
@@ -15,14 +15,23 @@ prompt_theme_installer() {
 theme_installer() {
   rootwd=$(pwd)
   read -r -p "==> Great! What name do you want to give to it? [A-Za-z0-9_] " themename
+  read -r -p "==> And what is the repository URL?  (git@bitbucket...)" repo
+
   composer require vincit/wordpress-theme-base dev-master --prefer-source
   cd htdocs/wp-content/themes || exit 1
   mv wordpress-theme-base "$themename"
+  cd "$themename" || exit 1
+
+  # git remote remove composer
+  git remote remove origin
+  git remote add origin "$repo"
 
   recursive_replace "." "*" "wordpress-theme-base" "$themename"
 
-
   cd "$rootwd" || exit 1
   recursive_replace "$rootwd" "composer.json" "wordpress-theme-base" "$themename"
+
+  echo "Theme generated. Assuming you provided a valid repository URL, you should be able to just push into it."
+  echo "Remember to add it to Private Packagist or Composer will refuse to work: https://packagist.com/orgs/vincit/packages/add"
 }
 
