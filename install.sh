d32703fe1e8f0b9dfdea21a896824c3b81030fd2
Christian Nikkanen <christian.nikkanen@vincit.com>
2017-08-22 16:42:14 +0300

    Carefully calculate the amount of spaces required

--- install.sh
+++ install.sh
@@ -6,12 +6,15 @@ main() {
   while true; do
   clear
   echo ""
-  echo -e "           Vincit WordPress Installer "
+  echo -e "          Vincit WordPress Installer         "
   echo -e " ┌──────────────────────────────────────────┐"
   echo -e " │    1   Run installer                     │"
   echo -e " │    2   Create a new theme                │"
+  echo -e " │    3   Create a new plugin               │"
+  echo -e " │    9   Show logo                         │"
+  echo -e " │    0   Exit                              │"
   echo -e " └──────────────────────────────────────────┘"
-  echo -e "      Select an item   -     0   Exit "
+  echo -e "               Select an option              "
   echo ""
   read -s -n1 choix
   case $choix in
@@ -24,6 +27,16 @@ main() {
       echo
       theme_installer
       exit
+      ;;
+    3)
+      echo
+      echo "Plugin wizard has not graduated yet. Check back later."
+      read -s -n1
+      ;;
+    9)
+      echo
+      cat customizations/logo.txt
+      read -s -n1
       ;;
     0)
         clear && exit
